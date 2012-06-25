ac61b53d1a323cb01148dc593cfa57e1331028ff
PixelPirate <nag.brive@gmail.com>
2012-06-25 23:06:56 +0200

    Add Placeholder for Gunship Battle event

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -477,6 +477,7 @@ void AddSC_boss_lord_marrowgar();       // Icecrown Citadel
 void AddSC_boss_lady_deathwhisper();
 void AddSC_boss_deathbringer_saurfang();
 void AddSC_boss_festergut();
+void AddSC_boss_gunship_battle();
 void AddSC_boss_rotface();
 void AddSC_boss_professor_putricide();
 void AddSC_boss_blood_prince_council();
@@ -1186,6 +1187,7 @@ void AddNorthrendScripts()
     AddSC_boss_lady_deathwhisper();
     AddSC_boss_deathbringer_saurfang();
     AddSC_boss_festergut();
+	AddSC_boss_gunship_battle();
     AddSC_boss_rotface();
     AddSC_boss_professor_putricide();
     AddSC_boss_blood_prince_council();
---
+++ src/server/scripts/Northrend/IcecrownCitadel/boss_gunship_battle.cpp
@@ -0,0 +1,29 @@
+#include "icecrown_citadel.h"
+
+enum eAchievements
+{
+	IM_ON_A_BOAT_10 = 4536,
+    IM_ON_A_BOAT_25 = 4612
+};
+
+enum Actions
+{
+	ACTION_INTRO_START = 1,
+    ACTION_BATTLE_EVENT = 2,
+};
+
+enum Spells
+{
+};
+
+enum Events
+{
+};
+
+enum Texts
+{
+};
+
+void AddSC_boss_gunship_battle()
+{
+}
