d07e5ae177d866c97997f1f5a3e91a707775411e
Pixel-Pirate <nag.brive@gmail.com>
2012-06-26 18:51:23 +0200

    Gunship/Core: Add spell to jump from ship to ship

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -3064,6 +3064,11 @@ void SpellMgr::LoadDbcDataCorrections()
             case 54171: // Divine Storm
                 spellInfo->MaxAffectedTargets = 3;
                 break;
+			case 68645: // Spell permettant de sauter d'un bateau à l'autre -- Gunship
+                spellInfo->Effect[0] = SPELL_EFFECT_KNOCK_BACK_DEST;
+                spellInfo->EffectMiscValue[0] = -250;
+                spellInfo->EffectBasePoints[0] = 150
+                break;
             case 38310: // Multi-Shot
             case 53385: // Divine Storm (Damage)
                 spellInfo->MaxAffectedTargets = 4;
