ba72e15417e48ae21a509ad7b459df89d180f11d
PixelPirate <nag.brive@gmail.com>
2012-06-27 18:04:33 +0200

    Gunship/Script: implementation part.07

--- src/server/scripts/Northrend/IcecrownCitadel/boss_gunship_battle.cpp
+++ src/server/scripts/Northrend/IcecrownCitadel/boss_gunship_battle.cpp
@@ -1,6 +1,9 @@
 #include "icecrown_citadel.h"
+#include "Vehicle.h"
+#include "Group.h"
 #include "Transport.h"
 #include "MapManager.h"
+
 
 enum eAchievements
 {
@@ -1264,10 +1267,259 @@ class npc_gunship_cannon : public CreatureScript
         }
 };
 
+class npc_korkron_axethrower_rifleman : public CreatureScript
+{
+    public:
+        npc_korkron_axethrower_rifleman() : CreatureScript("npc_korkron_axethrower_rifleman") { }
+
+        struct npc_korkron_axethrower_riflemanAI : public Scripted_NoMovementAI
+        {
+            npc_korkron_axethrower_riflemanAI(Creature *creature) : Scripted_NoMovementAI(creature),_instance(creature->GetInstanceScript())
+            {
+                Reset();
+            }
+
+            void Reset()
+            {
+                ScriptedAI::Reset();
+                DesperateResolve = RAID_MODE( SPELL_DESPERATE_RESOLVE_10_NM, SPELL_DESPERATE_RESOLVE_25_NM, SPELL_DESPERATE_RESOLVE_10_HM, SPELL_DESPERATE_RESOLVE_25_HM);
+                desperated = false;
+                me->RemoveAurasDueToSpell(SPELL_EXPERIENCED);
+                me->RemoveAurasDueToSpell(SPELL_ELITE);
+                me->RemoveAurasDueToSpell(SPELL_VETERAN);
+                events.ScheduleEvent(EVENT_EXPERIENCED, urand(19000, 21000));
+                events.ScheduleEvent(EVENT_VETERAN, urand(39000, 41000));
+                events.ScheduleEvent(EVENT_ELITE, urand(59000, 61000));
+                attacktimer = urand(6000, 15000);
+                if (me->GetEntry() == NPC_GB_SKYBREAKER_RIFLEMAN)
+                    SetEquipmentSlots(false, EQUIP_NO_CHANGE, EQUIP_NO_CHANGE, 47267);
+                else
+                    SetEquipmentSlots(false, 49691, EQUIP_NO_CHANGE, EQUIP_NO_CHANGE);
+            }
+
+            void DamageTaken(Unit* /*attacker*/, uint32& /*damage*/)
+            {
+                if (_instance->GetData(DATA_TEAM_IN_INSTANCE) == ALLIANCE && me->GetHealthPct() < 20.0f && !desperated)
+                {
+                    desperated = true;
+                    DoCast(me, DesperateResolve);
+                }
+            }
+
+            void JustDied(Unit* killer)
+            {
+                if (_instance->GetData(DATA_TEAM_IN_INSTANCE) == ALLIANCE)
+                {
+                    if (Creature* pSaurfangBoss = ObjectAccessor::GetCreature(*me, _instance->GetData64(DATA_GB_HIGH_OVERLORD_SAURFANG)))
+                        pSaurfangBoss->AI()->DoAction(ACTION_AXES_RIFL_DIE);
+                }
+                
+                if (_instance->GetData(DATA_TEAM_IN_INSTANCE) == HORDE)
+                {
+                    if (Creature* pMuradin = ObjectAccessor::GetCreature(*me, _instance->GetData64(DATA_GB_MURADIN_BRONZEBEARD)))
+                        pMuradin->AI()->DoAction(ACTION_AXES_RIFL_DIE);
+                }
+            }
+
+            void UpdateAI(const uint32 diff)
+            {
+                if (_instance->GetBossState(DATA_GUNSHIP_EVENT) != IN_PROGRESS)
+                    return;
+                
+                if (me->HasUnitState(UNIT_STAT_CASTING))
+                    return;
+                    
+                me->AI()->AttackStart(SelectRandomPlayerInTheMaps(me->GetMap()));
+
+                if (!UpdateVictim())
+                    return;
+
+                events.Update(diff);
+
+                if (attacktimer <= diff)
+                {
+                    if(me->GetEntry() == NPC_GB_KORKRON_AXETHROWER)
+                        DoCast(me->getVictim(), SPELL_HURL_AXE);
+                    else if(me->GetEntry() == NPC_GB_SKYBREAKER_RIFLEMAN)
+                        DoCast(me->getVictim(), SPELL_SHOOT);
+                    attacktimer = urand(6000, 15000);
+                } else attacktimer -= diff;
+
+                while (uint32 eventId = events.ExecuteEvent())
+                {
+                    switch (eventId)
+                    {
+                        case EVENT_EXPERIENCED:
+                            DoCast(me, SPELL_EXPERIENCED);
+                            break;
+                        case EVENT_VETERAN:
+                            me->RemoveAurasDueToSpell(SPELL_EXPERIENCED);
+                            DoCast(me, SPELL_VETERAN);
+                            break;
+                        case EVENT_ELITE:
+                            me->RemoveAurasDueToSpell(SPELL_VETERAN);
+                            DoCast(me, SPELL_ELITE);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+
+            uint32 DesperateResolve;
+            uint32 attacktimer;
+
+            private:
+                EventMap events;
+                bool desperated;
+                InstanceScript* _instance;
+        };
+
+        CreatureAI* GetAI(Creature* pCreature) const
+        {
+            return new npc_korkron_axethrower_riflemanAI(pCreature);
+        }
+};
+
+class npc_sergeant : public CreatureScript
+{
+    public:
+        npc_sergeant() : CreatureScript("npc_sergeant") { }
+
+        struct npc_sergeantAI : public ScriptedAI
+        {
+            npc_sergeantAI(Creature *creature) : ScriptedAI(creature),_instance(creature->GetInstanceScript())
+            {
+                Reset();
+            }
+
+            void Reset()
+            {
+                ScriptedAI::Reset();
+                me->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_DISABLE_MOVE);
+                me->SetReactState(REACT_PASSIVE);
+                events.ScheduleEvent(EVENT_WALK_MOBS, 1500);
+                desperated = false;
+                me->RemoveAurasDueToSpell(SPELL_EXPERIENCED);
+                me->RemoveAurasDueToSpell(SPELL_ELITE);
+                DesperateResolve = RAID_MODE( SPELL_DESPERATE_RESOLVE_10_NM, SPELL_DESPERATE_RESOLVE_25_NM, SPELL_DESPERATE_RESOLVE_10_HM, SPELL_DESPERATE_RESOLVE_25_HM);
+                events.ScheduleEvent(EVENT_EXPERIENCED, urand(19000, 21000)); 
+                events.ScheduleEvent(EVENT_VETERAN, urand(39000, 41000)); 
+                events.ScheduleEvent(EVENT_BURNING_PITCH, urand(60000, 62000));c
+                if(me->GetMap()->ToInstanceMap()->GetMaxPlayers() == 25 && me->GetMap()->IsHeroic())
+                    events.ScheduleEvent(EVENT_ELITE, urand(59000, 61000));
+            }
+
+            void DamageTaken(Unit* /*attacker*/, uint32& /*damage*/)
+            {
+                if (me->GetHealthPct() < 20.0f && !desperated)
+                {
+                    desperated = true;
+                    DoCast(me, DesperateResolve);
+                }
+            }
+
+            bool CanAIAttack(Unit const* target) const
+            {
+                if (target->GetEntry() == NPC_GB_GUNSHIP_HULL)
+                    return false;
+
+                return true;
+            }
+
+            void UpdateAI(const uint32 diff)
+            {
+                if(_instance->GetBossState(DATA_GUNSHIP_EVENT) != IN_PROGRESS)
+                    return;
+
+                if (me->HasUnitState(UNIT_STAT_CASTING))
+                    return;
+                    
+                events.Update(diff);
+
+                while (uint32 eventId = events.ExecuteEvent())
+                {
+                    switch (eventId)
+                    {
+                        case EVENT_WALK_MOBS:
+                            me->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_DISABLE_MOVE);
+                            me->SetReactState(REACT_AGGRESSIVE);
+                            switch (me->GetEntry())
+                            {
+                                case NPC_GB_KORKRON_SERGANTE:
+                                    if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 1, 0.0f, true, SPELL_ON_SKYBREAKERS_DECK))
+                                    {
+                                        me->GetMotionMaster()->MoveChase(target);
+                                        me->AI()->AttackStart(target);
+                                    }
+                                break;
+                                case NPC_GB_SKYBREAKER_SERGANTE:
+                                    if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 1, 0.0f, true, SPELL_ON_ORGRIMS_HAMMERS_DECK))
+                                    {
+                                        me->GetMotionMaster()->MoveChase(target);
+                                        me->AI()->AttackStart(target);
+                                    }
+                                break;
+                            }
+                            events.ScheduleEvent(EVENT_WOUNDING_STRIKE, 5000);
+                            events.ScheduleEvent(EVENT_BLADE_STORM, 3000);
+                            break;
+                        case EVENT_BURNING_PITCH:
+                            if (Unit* pTarget = SelectTarget(SELECT_TARGET_RANDOM, 0, 100.0f, true))
+                            {
+                                if (me->GetEntry() == NPC_GB_KORKRON_SERGANTE)
+                                    DoCast(pTarget, SPELL_BURNING_PITCH_H);
+                                else
+                                    DoCast(pTarget, SPELL_BURNING_PITCH_A);
+                            }
+                            events.ScheduleEvent(EVENT_BURNING_PITCH, urand(60000, 62000));// ~61 sec
+                            break;
+                        case EVENT_EXPERIENCED:
+                            DoCast(me, SPELL_EXPERIENCED);
+                            break;
+                        case EVENT_VETERAN:
+                            me->RemoveAurasDueToSpell(SPELL_EXPERIENCED);
+                            DoCast(me, SPELL_VETERAN);
+                            break;
+                        case EVENT_ELITE:
+                            me->RemoveAurasDueToSpell(SPELL_VETERAN);
+                            DoCast(me, SPELL_ELITE);
+                            break;
+                        case EVENT_WOUNDING_STRIKE:
+                            if (UpdateVictim())
+                                DoCastVictim( RAID_MODE( SPELL_WOUNDING_STRIKE_10_NM, SPELL_WOUNDING_STRIKE_25_NM, SPELL_WOUNDING_STRIKE_10_HM, SPELL_WOUNDING_STRIKE_25_HM));
+                            events.ScheduleEvent(EVENT_WOUNDING_STRIKE, urand(5000, 8000));
+                            break;
+                        case EVENT_BLADE_STORM:
+                            DoCast(me, SPELL_BLADESTORM);
+                            events.ScheduleEvent(EVENT_BLADE_STORM, urand(9000, 16000));
+                            break;
+                    }
+                }
+
+                DoMeleeAttackIfReady();
+
+            }
+                uint32 DesperateResolve;
+                bool desperated;
+
+            private:
+                EventMap events;
+                InstanceScript* _instance;
+        };
+
+        CreatureAI* GetAI(Creature* pCreature) const
+        {
+            return new npc_sergeantAI(pCreature);
+        }
+};
+
 void AddSC_boss_gunship_battle()
 {
 	new npc_muradin_gunship();
 	new npc_gunship_skybreaker();
 	new npc_gunship_orgrimmar();
 	new npc_gunship_cannon();
+	new npc_korkron_axethrower_rifleman();
+	new npc_sergeant();
 }
